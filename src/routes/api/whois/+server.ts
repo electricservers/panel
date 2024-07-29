1df798dd4ecfb5e815fdd0cbe5245f9636e44733
Maximiliano Jabase <maxijabase@gmail.com>
2024-07-29 03:02:51 -0300

    fix

--- src/routes/api/whois/+server.ts
+++ src/routes/api/whois/+server.ts
@@ -1,11 +1,11 @@
-import prisma from '$lib/prisma';
+import prismaArg from '$lib/prismaArg';
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async (event: any) => {
     const query = event.url.searchParams;
     const steamid = query.get('steamid');
-    const playerInfo = await prisma.whois_logs.findMany({
+    const playerInfo = await prismaArg.whois_logs.findMany({
         where: {
             steam_id: steamid
         }
